2a5b69b95e7c1f3b1cecf661f34431a65eac9d05
brunoBrizo <bbrizolara7@gmail.com>
2021-08-19 14:43:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gmail API with Flutter
+
+gmail test app
+
+## Getting Started
+
+For the project to work, you need two things:
+
+- register the project in [Firebase](https://console.firebase.google.com) and insert the `google-services.json` file into the path` android/app/google-services.json`
+- enable the [Gmail API](https://console.developers.google.com/apis) in your referring to the project created in Firebase
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_sign_in/google_sign_in.dart';
+import "package:http/http.dart" as http;
+import 'dart:convert';
+
+GoogleSignIn googleSignIn = new GoogleSignIn(
+  scopes: <String>['https://www.googleapis.com/auth/gmail.send'],
+);
+
+Future<Null> handleSignOut() async {
+  googleSignIn.disconnect();
+}
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  LoginPageState createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  Future<Null> testingEmail(String userId, Map header) async {
+    header['Accept'] = 'application/json';
+    header['Content-type'] = 'application/json';
+
+    var from = userId;
+    var to = userId;
+    var subject = 'test send email';
+    //var message = 'worked!!!';
+    var message = "Hi<br/>Html Email";
+    var content = '''
+Content-Type: text/html; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+to: $to
+from: $from
+subject: $subject
+
+$message''';
+
+    var bytes = utf8.encode(content);
+    var base64 = base64Encode(bytes);
+    var body = json.encode({'raw': base64});
+
+    String url = 'https://www.googleapis.com/gmail/v1/users/' +
+        userId +
+        '/messages/send';
+
+    final Uri uri = Uri.parse(url);
+    final http.Response response =
+        await http.post(uri, headers: header, body: body);
+    if (response.statusCode != 200) {
+      setState(() {
+        print('error: ' + response.statusCode.toString());
+        print(url);
+        print(json.decode(response.body));
+      });
+      return;
+    }
+    final Map<String, dynamic> data = json.decode(response.body);
+    print('ok: ' + response.statusCode.toString());
+    print(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var assetImage = new AssetImage('assets/google-g-logo.png');
+    var image = new Image(image: assetImage, width: 96.0, height: 96.0);
+
+    var loginPage = new Scaffold(
+        backgroundColor: Colors.pink[400],
+        body: new Center(
+            child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Material(
+                color: Colors.white,
+                type: MaterialType.circle,
+                elevation: 6.0,
+                child: new GestureDetector(
+                  child: new Container(
+                      width: 112.0,
+                      height: 112.0,
+                      child: new InkWell(
+                        onTap: () async {
+                          try {
+                            await googleSignIn.signIn().then((data) {
+                              data.authHeaders.then((result) {
+                                var header = {
+                                  'Authorization': result['Authorization'],
+                                  'X-Goog-AuthUser': result['X-Goog-AuthUser']
+                                };
+                                testingEmail(data.email, header);
+                              });
+                            });
+                          } catch (error) {
+                            print(error);
+                          }
+                        },
+                        child: new Center(child: image),
+                      )),
+                )),
+            new Container(
+              margin: new EdgeInsets.only(top: 16.0),
+              child: new Text(
+                'Sign up with Google',
+                style: new TextStyle(
+                    color: Colors.white, fontFamily: "Futura", fontSize: 18.0),
+              ),
+            ),
+          ],
+        )));
+
+    return loginPage;
+  }
+}
